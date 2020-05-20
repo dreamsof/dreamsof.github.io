53663ef6c08340d69b5f61777a51c289dd459c09
dreamsof <65149551+dreamsof@users.noreply.github.com>
2020-05-20 17:38:43 -0400

    Update languages.js

--- languages.js
+++ languages.js
@@ -29,35 +29,35 @@ const ELEMENTS = [
 
 const TRANSLATIONS = {
 	'es': [
-		/* text01:  intro */ 'haz tus posteos de instagram visibles usando los hashtags elegidos por nuestro algoritmo',
-		/* button1 */ 'DESCARGA LA HERRAMIENTA',
+		/* intro (text01) */ 'haz visibles tus posteos de instagram usando los hashtags elegidos por nuestro algoritmo',
+		/* button 1 */ 'DESCARGÁ LA HERRAMIENTA',
 
-		/* text06:  title section 1 */ 'DEJÁ DE USAR LOS HASHTAGS INCORRECTOS',
-		/* text07:  content section 1 */ 'No importa si eres influencer, fotógrafo o social media manager, al usar hashtags compites con otros posteos que buscan la misma audiencia que tú. Por ejemplo: al usar #Travel estarás enterrando tu publicación junto con millones de otros usuarios. Es por esto que debes seleccionar los hashtags de forma inteligente para lograr el mejor cocktail que potencie tu publicación.',
+		/* title section 1 (text06) */ 'DEJÁ DE USAR LOS HASHTAGS INCORRECTOS',
+		/* content section 1 (text07) */ 'No importa si eres influencer, fotógrafo o social media manager, al usar hashtags compites con otros posteos que buscan la misma audiencia que tú.<br/> <em>Por ejemplo:</em> al usar <strong>#Travel</strong> estarás enterrando tu publicación junto con millones de otros usuarios. Es por esto que debes seleccionar tus hashtags de forma inteligente para lograr el <strong>mejor cocktail</strong> que potencie tu publicación.',
 
-		/* text09:  title section 2 */ 'EFECTO CASCADA',
-		/* text10:  content section 2 */ 'Tus posteos merecen ser vistos. PUNTO. Solo introduce un par de hashtags semilla y deja que nuestro algoritmo elija por ti 30 hashtags perfectos para el cocktail. Donde cada uno de los hashtags seleccionados ayudará a generar el efecto cascada basado en la cantidad de posteos que haya en cada hashtag seleccionado.',
+		/* title section 2 (text09) */ 'EFECTO CASCADA',
+		/* content section 2 (text10) */ 'Tus posteos merecen ser vistos. PUNTO. Solo introduce un par de hashtags semilla y deja que <strong>nuestro algoritmo</strong> elija por ti <strong>30 hashtags perfectos</strong> para el cocktail. Donde cada uno de los hashtags seleccionados ayudará a generar el efecto cascada basado en la cantidad de posteos que haya en cada hashtag seleccionado.',
 
-		/* text03:  title section 3 */ 'HACKEA LA ESTRATEGIA DE HASHTAGS',
-		/* text12:  content section 3 */ 'Si seleccionas tus posteos de forma estratégica ¿porqué no haces lo mismo con los hashtags que usas? Tienes que ser metódico y elegir hashtags que permitan que tus publicaciones lleguen a ser top-performer en la mayoría de hashtags que usas. El cocktail de 30-hashtags brindado por la herramienta Just Tags permitirá que tus posteos sean top-performer en el 60% de los hashtags utilizados. Dándote mayor competitividad para los 40% de hashtags restantes (con más post compitiendo contigo). De esta forma tu posteo puede beneficiarse del efecto cascada.',
+		/* title section 3 (text03) */ 'HACKEA LA ESTRATEGIA DE HASHTAGS',
+		/* content section 3 (text12) */ 'Si seleccionas tus posteos de forma estratégica ¿porqué no haces lo mismo con los hashtags que usas? Tienes que ser metódico y elegir hashtags que permitan que tus publicaciones sean destacadas en la mayoría de hashtags que usas. El cocktail de 30-hashtags brindado por la herramienta <strong>Just Tags</strong> permitirá que <strong>tus posteos sean lideres</strong> en el 60% de los hashtags utilizados. Dándote mayor competitividad para los 40% de hashtags restantes (con más post compitiendo contigo). De esta forma tu posteo puede beneficiarse del efecto cascada.',
 
-		/* text02:  title */ 'SE INTELIGENTE, HAZTE VIRAL',
-		/* text04:  subtitle */ 'hackea la #estrategia con nuestro algoritmo ahora',
-		/* button2 */ 'EMPEZAR AHORA',
+		/* title (text02) */ 'SE INTELIGENTE, HAZTE VIRAL',
+		/* subtitle (text04) */ 'hackea la #estrategia con nuestro algoritmo ahora',
+		/* button 2 */ 'EMPEZÁ AHORA',
 
-		/*  text16: bio1 ...  */ '@josefina.mariap - diseñadora gráfica "2 días después de publicar mi primera foto con los hashtags que obtuve de la herramienta Just Tags, ¡el 59% de las personas que vieron la publicación no me seguían! Esto es impresionante."',
-		/*  text11: bio2 ...  */ '@benja.xp - blogger/fotógrafo "No puedo creer lo fácil que es conseguir que mis publicaciones sean más vistas con esta herramienta. Todo este tiempo perdido sin siquiera usar hashtags. Ahora en segundos obtengo 30 combinaciones bien pensadas de hashtags listos para expandir mi alcance. "',
-		/*  text17: bio3 ...  */ '@weremoto - Comunidad Remota "Si necesitan que su publicación se dispare sobre las impresiones de personas que no lo siguen, entonces deje de buscar, esto es lo que necesitan."',
+		/* bio1 (text16) */ '<strong>@josefina.mariap</strong> - diseñadora gráfica "2 días después de publicar mi primera foto con los hashtags que obtuve de la herramienta Just Tags, ¡el 59% de las personas que vieron la publicación no me seguían! Esto es impresionante."',
+		/* bio2 (text11) */ '<strong>@benja.xp</strong> - blogger/fotógrafo "No puedo creer lo fácil que es conseguir que mis publicaciones sean más vistas con esta herramienta. Todo este tiempo perdido sin siquiera usar hashtags. Ahora en segundos obtengo 30 combinaciones bien pensadas de hashtags listos para expandir mi alcance. "',
+		/* bio3 (text17) */ '<strong>@weremoto</strong> - Comunidad Remota "Si necesitan que su publicación se dispare sobre las impresiones de personas que no lo siguen, entonces deje de buscar, esto es lo que necesitan."',
 
-		/* button3: */ 'DESCARGA AHORA JUST TAGS',
+		/* button 3 */ 'DESCARGA JUST TAGS HOY',
 
-		/* text13:  step 1 */ '1er paso - descarga la herramienta',
-		/* text14:  step 2 */ '2do paso - inserta los hashtags semilla y copia los 30 hashtags que te entrega la herramienta listos para ser usados en tu post.',
-		/* text18:   step 3 */ '3er paso - monitorea los resultados en la página de insights de instagram. *captura de pantalla de un posteo de nuestros usuarios usando los hashtags generados con Just Tags'
+		/* step 1 (text13) */ '<strong>1er paso</strong> - descarga la herramienta',
+		/* step 2 (text14) */ '<strong>2do paso</strong> - inserta los hashtags semilla y copia los 30 hashtags que te entrega la herramienta listos para ser usados en tu post.',
+		/* step 3 (text18) */ '<strong>3er paso</strong> - monitorea los resultados en la página de insights de instagram.<br/><em>*captura de pantalla de un posteo de nuestros usuarios usando los hashtags generados con Just Tags</em>'
 	]
 };
 
-function getPreferedLlanguage() {
+function getPreferedLanguage() {
 	if (!!navigator && !!navigator.languages && navigator.languages.length > 0) {
 		return navigator.languages[0]; 
 	} else if (!!navigator && !!navigator.language) {
@@ -67,8 +67,28 @@ function getPreferedLlanguage() {
 	}
 }
 
+function setLanguage(language) {
+	if (language.length != 2) {
+		return;
+	}
+	updateLanguageTo(language);
+}
+
+function updateLanguageTo(language) {
+	var translation = TRANSLATIONS[language];
+	if (translation.length != ELEMENTS.length) {
+		console.log('wrong number of strings / elements');
+		return;
+	}
+	for (var i = 0; i < translation.length; i++) {
+		let element = document.getElementById(ELEMENTS[i]);
+		let translatedText = translation[i];
+		element.innerHTML = translatedText;
+	}
+}
+
 window.addEventListener("load", (event) => {
-	var language = getPreferedLlanguage();
+	var language = getPreferedLanguage();
 	console.log('language ' + language);
 
 	if (!(language in TRANSLATIONS)) {
@@ -80,17 +100,9 @@ window.addEventListener("load", (event) => {
 		if (!(language in TRANSLATIONS)) {
 			console.log('(2) no translation for: ' + language);
 			return;
+		} else {
+			console.log('fallback to: ' + language);
 		}
 	}
-
-	var translation = TRANSLATIONS[language];
-	if (translation.length != ELEMENTS.length) {
-		console.log('wrong number of strings / elements');
-		return;
-	}
-	for (var i = 0; i < translation.length; i++) {
-		let element = document.getElementById(ELEMENTS[i]);
-		let translatedText = translation[i];
-		element.textContent = translatedText;
-	}
+	updateLanguageTo(language);
 });
